6781fb6c0e6c87a6125878ced32f8d3af2575a91
Flerys11 <fleuryssh@gmail.com>
2023-11-21 08:29:02 +0100

    script

--- src/main/java/com/nba/statistic/Script/script.sql
+++ src/main/java/com/nba/statistic/Script/script.sql
@@ -16,3 +16,45 @@ create table joueur(
     idjoueur bigint primary key,
     nomjoueur text
 );
+
+create table poste(
+    idposte bigint primary key,
+    nomposte varchar(100)
+);
+
+create table detailequipe(
+    idequipe bigint REFERENCES equipe(id),
+    idjoueur bigint REFERENCES joueur(id),
+);
+
+create table saison(
+    idsaison bigint primary key,
+    nom varchar(50)
+);
+
+create table match(
+    idmatch bigint primary key,
+    idsaison bigint REFERENCES saison(id),
+    equipe1 bigint REFERENCES equipe(id),
+    equipe2 bigint REFERENCES equipe(id),
+    datedebut timestamp,
+    datefin timestamp,
+    score1 double precision,
+    score2 double precision
+);
+
+create table statistiqueparmatch(
+    idjoueur bigint REFERENCES joueur(id),
+    idmatch bigint REFERENCES match(id),
+    point1 double precision,
+    point2 double precision,
+    point3 double precision,
+    passedecisif double precision,
+    rd double precision,
+    ro double precision,
+    block double precision,
+    mpm time,
+    tnmpoint1 double precision, 
+    tnmpoint2 double precision,
+    tnmpoint3 double precision
+);
