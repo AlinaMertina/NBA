2d6f742f2b993a6c17da1bdb94a6dbc57df329a0
AlinaMertina <mertinaclaudietoto@gmail.com>
2023-11-21 10:04:16 +0300

    seconde

--- src/main/java/com/nba/statistic/Script/script.sql
+++ src/main/java/com/nba/statistic/Script/script.sql
@@ -1,1 +1,18 @@
 nbastatistic
+
+create table ville (
+    idville bigint primary key,
+    nomville text
+);
+
+create table equipe(
+    idequipe bigint primary key,
+    nomequipe text,
+    idville bigint,
+    FOREIGN KEY (idville)  REFERENCES ville(idville)
+);
+
+create table joueur(
+    idjoueur bigint primary key,
+    nomjoueur text
+);
